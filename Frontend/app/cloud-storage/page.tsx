6e05c12bdc6601a7b0c2638eb9c1b87f4bb0a51c
NikunjChamaria <nikunjchamaria1998@gmail.com>
2024-09-29 13:56:40 +0530

    new changes

--- Frontend/app/cloud-storage/page.tsx
+++ Frontend/app/cloud-storage/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import Header from "../components/Header";
 import { Rajdhani } from "next/font/google";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { useDispatch ,useSelector} from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { setFullResponse } from "../features/responseSlice";
 import Result from "../result/page";
+import { BASE_URL } from "../constant";
 
 const rajdhani = Rajdhani({
   subsets: ["latin"],
@@ -19,42 +18,39 @@ const Select = () => {
   const [accessId, setaccessId] = useState<string>("");
   const [accessKey, setaccessKey] = useState<string>("");
   const [awsRegion, setawsRegion] = useState<string>("");
-  const Response=useSelector((state:any)=>state);
-const [data,setData]=useState<any|null>(null);
-const [isLoading,setIsLoading]=useState(false);
+  const Response = useSelector((state: any) => state);
+  const [data, setData] = useState<any | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleCloudApi = () => {
     setIsLoading(true);
-   
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: `http://localhost:8000/aws?bucket_name=${bucketName}&aws_access_key_id=${accessId}&aws_secret_access_key=${accessKey}&aws_region=${awsRegion}`,
-  headers: { 
-    'accept': 'application/json'
-  }
-};
 
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: `${BASE_URL}/aws?bucket_name=${bucketName}&aws_access_key_id=${accessId}&aws_secret_access_key=${accessKey}&aws_region=${awsRegion}`,
+      headers: {
+        accept: "application/json",
+      },
+    };
 
-
-axios.request(config)
-.then((response) => {
-  setIsLoading(false)
-  var res=response.data ;
-  console.log(response);
-  setData(res);
-  console.log(Response);
-})
-.catch((error) => {
-  console.log(error);
-});
+    axios
+      .request(config)
+      .then((response) => {
+        setIsLoading(false);
+        var res = response.data;
+        console.log(response);
+        setData(res);
+        console.log(Response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const goBack=()=>{
-    window.location.href='select';
-  }
-
-  
+  const goBack = () => {
+    window.location.href = "select";
+  };
 
   return (
     <div className={`${rajdhani.className} flex flex-col min-h-screen`}>
@@ -62,53 +58,62 @@ axios.request(config)
         <div className=" inset-0 bg-gradient-to-b from-[rgba(255,0,0,0.3)] to-[rgba(0,0,0,0.3)]"></div>
         <Header header="Cloud Storage" />
         {isLoading && (
-  <div className="flex justify-center items-center absolute inset-0 mt-40">
-    <div className="text-center">
-      <div className="spinner mx-auto"></div>
-    </div>
-  </div>
-)}
-        {data===null && !isLoading && <div className="flex flex-col items-center justify-center h-full">
-          <div
-            className="p-10"
-            style={{ backgroundColor: "rgba(94, 246, 255, 0.25)" }}
-          >
-            <h1 className="text-[#5EF6FF] text-4xl mb-4">
-              ENTER THE FOLLOWING DETAILS
-            </h1>
-            <div className="flex flex-col space-y-6">
-              <input
-                type="text"
-                placeholder="Bucket name"
-                className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
-                value={bucketName}
-                onChange={(e) => setBucketName(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="AWS Access ID"
-                className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
-                value={accessId}
-                onChange={(e) => setaccessId(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="AWS Secret Access Key"
-                className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
-                value={accessKey}
-                onChange={(e) => setaccessKey(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="AWS Region"
-                className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
-                value={awsRegion}
-                onChange={(e) => setawsRegion(e.target.value)}
-              />
+          <div className="flex justify-center items-center absolute inset-0 mt-40">
+            <div className="text-center">
+              <div className="spinner mx-auto"></div>
             </div>
           </div>
-        </div>}
-        {data!==null && !isLoading && <Result risk_score={data['score']} images={data['images']} code={data['code']} texts={data['texts']}/>}
+        )}
+        {data === null && !isLoading && (
+          <div className="flex flex-col items-center justify-center h-full">
+            <div
+              className="p-10"
+              style={{ backgroundColor: "rgba(94, 246, 255, 0.25)" }}
+            >
+              <h1 className="text-[#5EF6FF] text-4xl mb-4">
+                ENTER THE FOLLOWING DETAILS
+              </h1>
+              <div className="flex flex-col space-y-6">
+                <input
+                  type="text"
+                  placeholder="Bucket name"
+                  className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
+                  value={bucketName}
+                  onChange={(e) => setBucketName(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="AWS Access ID"
+                  className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
+                  value={accessId}
+                  onChange={(e) => setaccessId(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="AWS Secret Access Key"
+                  className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
+                  value={accessKey}
+                  onChange={(e) => setaccessKey(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="AWS Region"
+                  className="p-4 rounded-lg bg-[#224957] text-white text-lg w-full"
+                  value={awsRegion}
+                  onChange={(e) => setawsRegion(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+        {data !== null && !isLoading && (
+          <Result
+            risk_score={data["score"]}
+            images={data["images"]}
+            code={data["code"]}
+            texts={data["texts"]}
+          />
+        )}
       </main>
       <footer className="p-5 w-full flex justify-between items-center">
         <div className="flex gap-2 items-center">
@@ -122,9 +127,9 @@ axios.request(config)
         <button
           className="items-center flex px-12 h-16 text-3xl border border-[#5EF6FF] glow-on-hover"
           onClick={() => {
-            if(data===null){
+            if (data === null) {
               handleCloudApi();
-            }else{
+            } else {
               goBack();
             }
           }}
